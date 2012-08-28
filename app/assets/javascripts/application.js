af0e59376836d51387faa359301b2441393909b4
Tianlong Wu <rockuw@gmail.com>
2012-08-28 23:36:56 +0800

    finished Task F; Todo: 1. find out how to test ajax; 2. add blind_up effect when empty the cart;

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui-1.8.23.custom.min
 //= require_tree .
--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -21,7 +21,16 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, :product_id => products(:one).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, :product_id => products(:one).id
+    end
+
+    assert_response :success
+    # doesn't know how to test ajax response
   end
 
   test "should show line_item" do
