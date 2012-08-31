3350b79881536311d9e52c73ecd26356e449a105
Tianlong Wu <rockuw@gmail.com>
2012-08-31 13:03:57 +0800

    finished Task I

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829083414) do
+ActiveRecord::Schema.define(:version => 20120831023135) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(:version => 20120829083414) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
---
+++ test/functional/users_controller_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    @new_user = @user.dup
+    @new_user.name = "newuser"
+
+    assert_difference('User.count') do
+      post :create, user: { hashed_password: @new_user.hashed_password, name: @new_user.name, salt: @new_user.salt }
+    end
+
+    assert_redirected_to users_path
+  end
+
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should update user" do
+    put :update, id: @user, user: { hashed_password: @user.hashed_password, name: @user.name, salt: @user.salt }
+    assert_redirected_to users_path
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+
+    assert_redirected_to users_path
+  end
+end
